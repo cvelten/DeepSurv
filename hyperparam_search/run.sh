5cd07e338af3be80c822c9e4d5c471b22cbff5b9
Christian Velten <cv2415@columbia.edu>
2022-01-28 08:16:15 -0500

    output

--- hyperparam_search/run.sh
+++ hyperparam_search/run.sh
@@ -2,5 +2,6 @@
 echo "" > log-compose
 echo "" > log-err-compose
 for f in docker-compose_*; do
+	echo "docker-compose for ${f}"
 	docker-compose -f $f up --build >log-compose 2>log-err-compose
 done
